903da7df00040016b27b0df3ffbf66926f3174c0
Артём <catsoup1337@yandex.ru>
2020-09-13 17:56:13 +0300

    v2

--- homework.py
+++ homework.py
@@ -24,15 +24,15 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    params = {'from_date':0}
+    params = {'from_date': current_timestamp}
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     homework_statuses = requests.get(URL, headers=headers, params=params)
     return homework_statuses.json()
 
 
-def send_message(parse_homework_status):
+def send_message(message):
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    return bot.send_message(chat_id = CHAT_ID, text = str(parse_homework_status()))
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
